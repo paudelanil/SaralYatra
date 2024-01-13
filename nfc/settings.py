dfcaaa7df9bd5ab86eedd405fe18b46f7bc1dd20
paudelanil <paudelanil9@gmail.com>
2024-01-13 19:58:21 +0545

    cleaned code

--- nfc/settings.py
+++ nfc/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "reader.apps.ReaderConfig",
-    "django_extensions",
+    # "django_extensions",
 ]
 
 MIDDLEWARE = [
